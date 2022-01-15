4cdf45ad7e584c8bcb8d32037b67211aa7955682
ymtk00 <ymbtkyk@yahoo.co.jp>
2022-01-16 02:47:19 +0900

    エラー修正

--- src/components/footer-icon1.jsx
+++ src/components/footer-icon1.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import Image from 'next/image';
+import React from 'react';
 
 class FooterIcon1 extends React.Component {
   render() {
     return (
       <div>
-        <Image src={this.props.image} alt="fwywd" width={85} height={100}/>
+        <Image src={this.props.image} alt='fwywd' width={85} height={100} />
       </div>
     );
   }
