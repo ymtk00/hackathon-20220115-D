ebeb0eba30763086cc35ac530e079cae2c061afd
鈴木 貴典 <suzukitakanori@suzukinoMacBook-Pro.local>
2022-01-15 16:01:31 +0900

    commit

---
+++ src/components/main-bg.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// // import Image from 'next/image';
+
+// class MainBg extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className='main-bg'>
+    
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default MainBg;
---
+++ src/components/sample/sample-main-bg.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import Image from 'next/image';
+
+// class MainBg extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className='main-bg'>
+//           <Image
+//             src={this.props.image}
+//             alt="bg"
+//             width={100}
+//             height={100}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default MainBg;
