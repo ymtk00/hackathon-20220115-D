0f6e546f9b5c3bb56dc6974214abc3a90311c74e
suzuki57 <6450.tk.0507@gmail.com>
2022-02-05 20:38:14 +0900

    ninth-commit

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero(): JSX.Element {
   return (
       <div className="z-0">
         <div className="relative">
-          <div className="-mb-44 h-[1150px] bg-[url('/images/hero.png')] bg-cover"></div>
+          <div className="-mb-44 h-[1150px] bg-[url('/images/hero.WebP')] bg-cover"></div>
           <div className="w-[1680px] h-[180px] bg-[url('/images/Vector1.png')] bg-cover"></div>
         </div>
         <div className="absolute top-[500px] left-[700px]">
